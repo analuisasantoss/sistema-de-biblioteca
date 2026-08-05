d54c3b3c866a9471abc18994add0dc3c0f15f38e
Rafael dos Santos silva <rafael.silva@redepalacio.net>
2026-08-05 20:35:02 -0300

    alterações em como salva os dados

--- biblioteca.py
+++ biblioteca.py
@@ -1,47 +1,27 @@
 livros = []
-while True:
-    print("SISTEMA DA BIBLIOTECA")
-    print("1 - Cadastrar livros")
-    print("2 - Registrar empréstimo")
-    print("3 - Registrar devolução")
-    print("4 - Listar livros")
-    print("5 - Buscar livro")
-    print("6 - Ordenar livros")
-    print("7 - Sair")
-    opcao = input("Escolha uma opção: ")
-    if opcao == "1":
-        # Cadastro de livros
-        print("CADASTRAR LIVRO")
-        titulo = input("Título: ")
-        autor = input("Autor: ")
-        ano = input("Ano de publicação: ")
-        isbn = input("Código/ISBN: ")
-        status = "disponível"
-        livro = {
-            "titulo": titulo,
-            "autor": autor,
-            "ano": ano,
-            "isbn": isbn,
-            "status": status
-        }
-        livros.append(livro)
-        arquivo = open("livros.csv", "a", encoding="utf-8")
-        arquivo.write(titulo + "," + autor + "," + ano + "," + isbn + "," + status + "\n")
-        arquivo.close()
-        print("Livro cadastrado com sucesso!")
-        print("Arquivo salvo!")
-    elif opcao == "2":
-        print("REGISTRAR EMPRÉSTIMO")
-    elif opcao == "3":
-        print("REGISTRAR DEVOLUÇÃO")
-    elif opcao == "4":
-        print("LISTAR LIVROS")
-    elif opcao == "5":
-        print("BUSCAR LIVRO")
-    elif opcao == "6":
-        print("ORDENAR LIVROS")
-    elif opcao == "7":
-        print("Sistema encerrado!")
-        break
-    else:
-        print("Opção inválida!")
+# Recupera os livros salvos no arquivo quando o programa inicia
+arquivo = open("livros.csv", "r", encoding="utf-8")
+for linha in arquivo:
+    dados = linha.split(",")
+    livro = {
+        "titulo": dados[0],
+        "autor": dados[1],
+        "ano": dados[2],
+        "isbn": dados[3],
+        "status": dados[4].replace("\n", "")
+    }
+    livros.append(livro)
+arquivo.close()
+# Salva os livros no arquivo para manter os dados guardados
+def salvar_arquivo():
+    arquivo = open("livros.csv", "w", encoding="utf-8")
+    for livro in livros:
+        arquivo.write(
+            livro["titulo"] + "," +
+            livro["autor"] + "," +
+            livro["ano"] + "," +
+            livro["isbn"] + "," +
+            livro["status"] + "\n"
+        )
+    arquivo.close()
+ 
