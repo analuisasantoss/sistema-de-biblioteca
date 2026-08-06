64aea040d0932a4801063b7b3278b644a7f6d986
Rafael dos Santos silva <rafael.silva@redepalacio.net>
2026-08-05 22:26:29 -0300

    emprestar livro e cadastro

--- biblioteca.py
+++ biblioteca.py
@@ -24,4 +24,67 @@ def salvar_arquivo():
             livro["status"] + "\n"
         )
     arquivo.close()
+    # Cadastro de livros 
+def cadastrar_livro():
+    print("cadastrar livro")
+    titulo = input("Título: ")
+    autor = input("Autor: ")
+    ano = input("Ano de publicação: ")
+    isbn = input("Código/ISBN: ")
+    status = "disponível"
+    livro = {
+        "titulo": titulo,
+        "autor": autor,
+        "ano": ano,
+        "isbn": isbn,
+        "status": status
+    }
+    livros.append(livro)
+    salvar_arquivo()
+    return "Livro cadastrado!"
+ # Listar todos os livros cadastrados mostrando seus dados e status
+def listar_livros():
+    print("listar livros")
+    for livro in livros:
+        print("Título:", livro["titulo"])
+        print("Autor:", livro["autor"])
+        print("Ano:", livro["ano"])
+        print("ISBN:", livro["isbn"])
+        print("Status:", livro["status"])
+    return "Lista finalizada"
+# Buscar um livro pelo título ou pelo autor
+def buscar_livro():
+    print("buscar livro")
+    busca = input("Digite o título ou autor: ")
+    for livro in livros:
+        if busca == livro["titulo"] or busca == livro["autor"]:
+            print("Título:", livro["titulo"])
+            print("Autor:", livro["autor"])
+            print("Ano:", livro["ano"])
+            print("ISBN:", livro["isbn"])
+            print("Status:", livro["status"])
+            return "Livro encontrado"
+    return "Livro não encontrado"
+ # Menu principal que mantém o sistema funcionando até o usuário escolher sair
+while True:
  
+    print("\nSISTEMA DA BIBLIOTECA")
+    print("1 - Cadastrar livros")
+    print("2 - Registrar empréstimo")
+    print("3 - Registrar devolução")
+    print("4 - Listar livros")
+    print("5 - Buscar livro")
+    print("6 - Ordenar livros")
+    print("7 - Sair")
+ 
+ 
+    opcao = input("Escolha uma opção: ")
+ 
+ 
+    if opcao == "1":
+ 
+        print(cadastrar_livro())
+ 
+ 
+    
+    
